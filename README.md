692704abb2aeb06e4e1554ce0f3bce51239208a4
mborges-pivotal <marcelo.marco.borges@gmail.com>
2022-04-28 17:13:10 -0500

    add option to test DSE drivers

--- README.md
+++ README.md
@@ -2,12 +2,25 @@
 
 Simple SpringBoot program to exercise Java Driver capabilities. 
 
-Build and Run
+## Build and Run
 ```
 $ ./mvnw clean install
 $ ./mvnw spring-boot:run
 ```
 
+## Endpoints
+Once the application is running, by default on port 8080, you can hit the endpoints below to test either the DSE or OSS driver.
+
+```
+/api/dse/version 
+/api/dse-oss/version
+```
+
+## For Cassandra OSS drivers
 Before building you can update the [application.conf](https://github.com/mborges-pivotal/dse-java-driver/blob/main/src/main/resources/application.conf) file with the java driver settings you need to connect to your cluster. 
 
 In the [src/main/resources](https://github.com/mborges-pivotal/dse-java-driver/tree/main/src/main/resources) you can find the default configuration files, including the application.conf and logback.xml. 
+
+## For DSE Drivers
+For DSE Driver we are using the SpringBoot [application.properties](https://github.com/mborges-pivotal/dse-java-driver/blob/main/src/main/resources/application.properties) file. 
+[Java Driver for DataStax Enterprise Manual](https://docs.datastax.com/en/developer/java-driver-dse/2.3/) For more information
--- src/main/java/com/datastax/workshop/drivers/DemoDSEController.java
+++ src/main/java/com/datastax/workshop/drivers/DemoDSEController.java
@@ -3,10 +3,13 @@ package com.datastax.workshop.drivers;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.dse.DseCluster;
+import com.datastax.driver.dse.DseSession;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoDSEController {
 
-    private CqlSession session;
+    @Value("${dsedriver.contact-point}")
+    private String contactPoint;
+
+    @Value("${dsedriver.local-dc}")
+    private String dc;
+
+    private DseCluster cluster;
+    private DseSession session;
 
     @GetMapping("/version")
     public String version() {
+        System.out.println("*** DSE Driver session query ***");
         ResultSet rs = session.execute("select release_version from system.local");
         Row row = rs.one();
         assert row != null;
@@ -26,18 +37,20 @@ public class DemoDSEController {
         return String.format("Cassandra version is: %s%n", releaseVersion);
     }
 
-
     @PostConstruct
     private void postConstruct() {
-        System.out.println("PostConstruct: Creating cqlSession object to DSE");
-         session = CqlSession.builder().build();
+        System.out.println("PostConstruct: Creating DseCluster and DseSession objects to DSE");
+        System.out.println("PostConstruct: contactPoint: " + contactPoint);
+        cluster = DseCluster.builder().addContactPoint(contactPoint).withoutMetrics()
+                .withLoadBalancingPolicy(DCAwareRoundRobinPolicy.builder().withLocalDc(dc).build()).build();
+        session = cluster.connect();
     }
 
     @PreDestroy
     public void cleanup() {
-        System.out.println("PreDestroy: Closing sqlSession object from DSE");
-        if (null != session) {
-            session.close();
+        System.out.println("PreDestroy: Closing DseCluster object from DSE");
+        if (null != cluster) {
+            cluster.close();
         }
     }
 
